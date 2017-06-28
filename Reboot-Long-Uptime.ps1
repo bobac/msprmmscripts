d7ac3924f487fc12d192de56d290a1aeb3ac9797
Robert Houser <bobac@bobac.net>
2017-06-28 15:01:36 +0200

    Initial commit - reboot long running machine

---
+++ Reboot-Long-Uptime.ps1
@@ -0,0 +1,21 @@
+param(
+$DaysTreshold = 10,
+$WarningTimeSec = 60
+)
+$OS = Get-WmiObject win32_operatingsystem
+$BootTime = $OS.ConvertToDateTime($OS.LastBootUpTime)
+$LocalTime = $OS.ConvertToDateTime($OS.LocalDateTime)
+$UpTime = $LocalTime - $BootTime
+$UpTime
+echo "Days Treshold: $DaysTreshold"
+echo "Warning Time: $WarningTimeSec"
+if ($UpTime.Minutes -ge $DaysTreshold)
+{
+  echo "reboot"
+  shutdown /r /t $WarningTimeSec
+}
+else
+{
+  echo "noreboot"
+}
+ 
