9257091db0adb49b6aa51fa210090f24ec56949d
Robert Houser <bobac@bobac.net>
2017-06-28 15:17:09 +0200

    Added README.md and fixed Days in uptime comparison

--- Reboot-Long-Uptime.ps1
+++ Reboot-Long-Uptime.ps1
@@ -1,6 +1,6 @@
 param(
 $DaysTreshold = 10,
-$WarningTimeSec = 60
+$WarningTimeSec = 300
 )
 $OS = Get-WmiObject win32_operatingsystem
 $BootTime = $OS.ConvertToDateTime($OS.LastBootUpTime)
@@ -9,7 +9,7 @@ $UpTime = $LocalTime - $BootTime
 $UpTime
 echo "Days Treshold: $DaysTreshold"
 echo "Warning Time: $WarningTimeSec"
-if ($UpTime.Minutes -ge $DaysTreshold)
+if ($UpTime.Days -ge $DaysTreshold)
 {
   echo "reboot"
   shutdown /r /t $WarningTimeSec
